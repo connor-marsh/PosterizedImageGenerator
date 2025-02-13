715d8e76907d63049a1a5a8d0bbcff7f2fd4f316
connor-marsh <connorbmarsh1000@gmail.com>
2025-02-13 16:46:53 -0500

    made posterize do grayscale also

--- script.js
+++ script.js
@@ -171,11 +171,27 @@ function posterizeImage() {
 
     dt = image1d2d(data, cols);
 
+    // convert to grayscale
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            var avg = dt[i][j][0] + dt[i][j][1] + dt[i][j][2];
+            avg /= 3;
+            dt[i][j][0] = avg;
+            dt[i][j][1] = avg;
+            dt[i][j][2] = avg;
+        }
+    }
+
     // Posterize it
     var ranges = [];
-    stepSize = 255 / numSteps;
+    stepSize = 256 / numSteps;
     for (let i = 0; i < numSteps; i++) {
         ranges.push([i * stepSize, (i + 1) * stepSize - 1]);
+    }
+    var values = []
+    stepSizeAlt = 256 / (numSteps - 1);
+    for (let i = 0; i < numSteps; i++) {
+        values.push(i * stepSizeAlt);
     }
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
@@ -186,7 +202,7 @@ function posterizeImage() {
                         range = l;
                     }
                 }
-                dt[i][j][k] = ranges[range][0];
+                dt[i][j][k] = values[range];
             }
         }
     }
@@ -214,43 +230,3 @@ function posterizeImage() {
     imageContainer.appendChild(newImage);
 }
 
-/*
-document.addEventListener('DOMContentLoaded', () => {
-    const fileInput = document.getElementById('imageInput');
-    const imageDisplay = document.getElementById('imageDisplay');
-
-    fileInput.addEventListener('change', handleImageUpload);
-
-    function handleImageUpload(event) {
-        const file = event.target.files[0];
-        if (file && file.type.startsWith('image/')) {
-            const reader = new FileReader();
-            reader.onload = function (e) {
-                const img = new Image();
-                img.src = e.target.result;
-
-                img.onload = function () {
-                    const canvas = document.createElement('canvas');
-                    const ctx = canvas.getContext('2d');
-                    canvas.width = img.width;
-                    canvas.height = img.height;
-                    ctx.drawImage(img, 0, 0);
-
-                    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                    const data = imageData.data;
-                    for (let i = 0; i < data.length; i += 4) {
-                        const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-                        data[i] = avg;
-                        data[i + 1] = avg;
-                        data[i + 2] = avg;
-                    }
-                    ctx.putImageData(imageData, 0, 0);
-                    imageDisplay.src = canvas.toDataURL();
-                };
-            };
-            reader.readAsDataURL(file);
-        } else {
-            alert("Please select a valid image file.");
-        }
-    }
-});*/
